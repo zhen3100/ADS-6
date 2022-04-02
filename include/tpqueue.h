50d9cedfd2f3ed2147ed2d7c9e19e61a8722bba7
anton <ashtanyuk@gmail.com>
2022-04-02 20:28:59 +0300

    Initial commit

---
+++ include/tpqueue.h
@@ -0,0 +1,15 @@
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+template<typename T, int size>
+class TPQueue {
+
+};
+
+struct SYM {
+  char ch;
+  int prior;
+};
+
+#endif  // INCLUDE_TPQUEUE_H_
